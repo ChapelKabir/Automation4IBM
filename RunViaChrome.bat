89cbf964b28228777413f2ded34f698fa64c7f49
ChapelKabir <36269622+ChapelKabir@users.noreply.github.com>
2018-04-03 14:51:39 +0100

    Add files via upload

---
+++ RunViaChrome.bat
@@ -0,0 +1,1 @@
+katalon -noSplash  -runMode=console -projectPath="C:\Users\mkabir\Katalon Studio\Automation Practice 1\Automation Practice 1.prj" -retry=0 -testSuitePath="Test Suites/Testing via Chrome" -browserType="IE"
